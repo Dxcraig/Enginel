3e9e6225dafdf44906490418f004dc01c33fa0b9
Dxcraig <steviedoe20@gmail.com>
2025-12-17 21:43:52 +0000

    Remove AuthContext, validation rules, ESLint configuration, TypeScript types, and various configuration files; delete unused SVG assets and clean up package dependencies.

--- enginel_website/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css' {
-    const content: { [className: string]: string };
-    export default content;
-}
-
-declare module 'react-datepicker/dist/react-datepicker.css';
-declare module '@/app/datepicker.css';
